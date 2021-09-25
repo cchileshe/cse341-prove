15c1e4ebe39319d30afeecce2d33460189a7d2bc
cchileshe <chi06001@byui.edu>
2021-09-25 16:01:54 +0200

    04 Teach: Team Activity

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const errorController = require('./controllers/error');
 
 const app = express();
 
+
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://cse341-proves.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -22,3 +30,26 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(5000);
+
+
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://admin:<password>@cse341.sm7ru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+    // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
